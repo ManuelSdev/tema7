1166b3af4e0e7bba72e4b7d572af3b934d29f265
Manuel S <masanchezzm@gmail.com>
2021-03-29 17:41:39 +0200

    creamos custom element para consumir plantilla

--- 04-custom-popup V1/main.js
+++ 04-custom-popup V1/main.js
@@ -1,6 +1,24 @@
 
-
+/*
+comento este paso inicial de consumo de plantilla sin cElement para crear ahora un cElement
 //Referencio el contenido de la plantilla
 const template = document.getElementById('plantilla');
 //Importo el clonado del contenido de la plantilla al documento/body directamente 
 document.body.appendChild(document.importNode(template.content, true));
+*/
+class KcPopup extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      const template = document.getElementById('plantilla');
+      const node = document.importNode(template.content, true);
+      this.appendChild(node);
+    }
+  }
+  
+  if (!customElements.get('kc-popup')) {
+    customElements.define('kc-popup', KcPopup);
+  }
+  
